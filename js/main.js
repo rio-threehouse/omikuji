5c135b5d65eb94ace68bfa2ed95093feea0fdcd7
Rio Sanka <rio@RionoMacBook.local>
2019-06-28 16:54:29 +0900

    確率を追加

--- js/main.js
+++ js/main.js
@@ -3,14 +3,22 @@
 {
   const btn = document.getElementById('btn');
     btn.addEventListener('click', () => {
-      // 配列取得
-      const results = ['大吉', '吉', '凶', '末吉'];
-      // クリックしたら乱数を取得 個数は配列の数にするlength
-      const n = Math.floor(Math.random() * results.length);
-            
-      btn.textContent = results[n];
-    });
+    // 確率を加える
+    const n = Math.random();
 
+      if (n < 0.1) {
+        btn.textContent = "凶" ;//10%
+      } else if (n < 0.4) { 
+        btn.textContent = '末吉'; //30%
+      } else if (n < 0.7) {
+        btn.textContent = '吉'; //30%
+      } else {
+        btn.textContent = '大吉';  //30%
+      }
+
+    });
+      
+      
     // マウスダウンした時にプレスをつける
     btn.addEventListener('mousedown', () => {
       btn.classList.add('pressed');
