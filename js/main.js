b2508203832aae53eb5ff2e1900ffac5405f9ae9
Rio Sanka <rio@RionoMacBook.local>
2019-06-28 16:41:01 +0900

    配列に変更

--- js/main.js
+++ js/main.js
@@ -3,22 +3,12 @@
 {
   const btn = document.getElementById('btn');
     btn.addEventListener('click', () => {
-      // クリックしたら乱数を取得
-      const n = Math.floor(Math.random() * 3);
-      // btn.textContent = n;
-
-      // 乱数に名前をつける
-      switch (n) {
-        case 0:
-          btn.textContent = '大吉';
-          break;
-        case 1:
-          btn.textContent = '吉';
-          break;
-        case 2:
-          btn.textContent = '凶';
-          break;
-      }
+      // 配列取得
+      const results = ['大吉', '吉', '凶', '末吉'];
+      // クリックしたら乱数を取得 個数は配列の数にするlength
+      const n = Math.floor(Math.random() * results.length);
+            
+      btn.textContent = results[n];
     });
 
     // マウスダウンした時にプレスをつける
